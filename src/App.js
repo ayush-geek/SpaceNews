dd916910533790da306af34860fb9f9483f43377
ayush-geek <aymalviya127@gmail.com>
2023-03-30 12:35:58 +0530

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useEffect ,useState } from 'react';
 
 function App() {
+
+    const [newsList ,setNewsList]=useState([]);
+  useEffect(()=>{
+    fetch("https://api.spaceflightnewsapi.net/v4/articles/").then((res)=>res.json())
+    .then((data)=>
+    {
+      setNewsList(data.results);
+    }
+    )
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="title">
+        <h1>Space News</h1>
+      </div>
+
+      <div className="newsContainer">
+        {
+          newsList.map((value,key)=>{
+            return <div key={key} className="article"
+            onClick={()=>{
+              window.location.href=value.url
+            }}>
+              <h3>{value.title}</h3>
+              <img src={value.image_url}></img>
+              <p>{value.summary}</p>
+              <h4>{value.published_at}</h4>
+              </div>
+          })
+
+        }
+      </div>
     </div>
   );
 }
